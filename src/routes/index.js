3d3d57e214d26e75ea49929085d619ff5df469cc
Dede Supriatna <dedesupriatna1421@gmail.com>
2025-02-13 11:17:00 +0700

    test: delete all

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,13 @@
 import express from "express";
-import projectRoutes from "./projectRoutes.js";
-import heroRoutes from "./heroRoutes.js";
 import educationRoutes from "./educationRoutes.js";
-import authRoutes from "./authRoutes.js"
 import userRoutes from "./userRoutes.js"
 import contactRouters from "./contactRoutes.js"
-import skillRoutes from "./skillRoutes.js"
 
 const router = express.Router();
 
-router.use("/project",  projectRoutes);
-router.use("/hero", heroRoutes);
 router.use("/education", educationRoutes)
-router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
 router.use("/contact", contactRouters); 
-router.use("/skill", skillRoutes)
 
 
 
