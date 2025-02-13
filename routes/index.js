bca2a69ac100e6d4e0a3fffcdf3790d75e399c5c
muhamademiralfani <muhamademiralfani@gmail.com>
2025-02-13 20:49:14 +0700

    feat(contactHandling): add contact Handling

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import educationRoutes from "./educationRoutes.js";
 import heroRoutes from "./heroRoutes.js";
+import contactRoutes from './contactRoutes.js'
 
 const router = express.Router();
 
 router.use("/education", educationRoutes);
 router.use("/hero", heroRoutes);
+
+router.use('/contact', contactRoutes)
 
 export default router;
