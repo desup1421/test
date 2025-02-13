11f28379dd32921ed3010036016bda99cad0a8a5
Dede Supriatna <dedesupriatna1421@gmail.com>
2025-02-13 17:49:18 +0700

    feat: add create and get hero controller

---
+++ routes/heroRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  getHero,
+//   updateHero,
+  createHero,
+} from "../controllers/heroController.js";
+import { uploadSingle } from "../middleware/uploadImageMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", getHero);
+router.post("/", uploadSingle, createHero);
+// router.put("/", uploadSingle, updateHero);
+
+export default router;
