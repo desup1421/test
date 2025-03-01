7ba6cef9da45bb4a968a3951920a0ed5deb21de6
ikokywu <rikiridwan949@gmail.com>
2025-03-01 13:22:02 +0700

    feat(models): add apiKey field to model

--- models/skillModel.js
+++ models/skillModel.js
@@ -1,19 +1,24 @@
 import mongoose from "mongoose";
 
-const skillSchema = new mongoose.Schema ({
-    name: {
-        type: String,
-        required: true
-    }, 
-    image : {
-        type: String,
-        required: false
-    }, 
-    level : {
-        type: String,
-        required: false
-    }
+const skillSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  level: {
+    type: String,
+    required: false,
+  },
+  apiKey: {
+    type: String,
+    required: true,
+    index: true,
+  },
 });
 
 const Skills = mongoose.model("Skills", skillSchema);
-export default Skills
+export default Skills;
